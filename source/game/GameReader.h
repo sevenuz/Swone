cfc4b75759aeb62ebf10d691f09856e97d8bfc32
sevenuz <julius@athenstaedt.net>
2021-01-21 11:06:54 +0100

    GameReader and Helper to namespaces

--- source/game/GameReader.h
+++ source/game/GameReader.h
@@ -11,14 +11,7 @@
 
 class Map;
 
-class GameReader {
-public:
-	// Not posible to create Objects of this class
-	GameReader() = delete;
-	~GameReader() {};
-	GameReader(GameReader const&) = delete;
-	void operator=(GameReader const&) = delete;
-
+namespace GameReader {
 	static constexpr const char* GFC_FILE_EXTENSION = "Gfc";
 
 	static constexpr const char* RES_DIR_MAP = "map/";
@@ -26,46 +19,46 @@ public:
 	static constexpr const char* RES_DIR_SCENERY = "scenery/";
 	static constexpr const char* RES_DIR_TEXTURE = "texture/";
 
-	static StringMapMap& getSceneryMap(std::string file);
-	static Map* getMap(std::string file, std::function<std::string(std::string textureName)> textureMapper);
-	static StringMapMap& getGameObjectMap(std::string file);
+	StringMapMap& getSceneryMap(std::string file);
+	Map* getMap(std::string file, std::function<std::string(std::string textureName)> textureMapper);
+	StringMapMap& getGameObjectMap(std::string file);
 
-	static std::string getTexturePath(std::string resDir, std::string name = "");
-	static std::string getSceneryPath(std::string resDir, std::string name = "");
-	static std::string getMapPath(std::string resDir, std::string name = "");
-	static std::string getGameObjectPath(std::string resDir, std::string name = "");
+	std::string getTexturePath(std::string resDir, std::string name = "");
+	std::string getSceneryPath(std::string resDir, std::string name = "");
+	std::string getMapPath(std::string resDir, std::string name = "");
+	std::string getGameObjectPath(std::string resDir, std::string name = "");
 
-	static void readSceneryMaps(std::string resDir);
-	static void hashResDir(std::string resDir);
+	void readSceneryMaps(std::string resDir);
+	void hashResDir(std::string resDir);
 
-	static std::string getHash(std::string file);
-	static std::string getFile(std::string hash);
+	std::string getHash(std::string file);
+	std::string getFile(std::string hash);
 
-	static const sf::Texture* loadTexture(std::string path);
-	static const sf::Image* loadImage(std::string path);
+	const sf::Texture* loadTexture(std::string path);
+	const sf::Image* loadImage(std::string path);
 
 	// is only once created and will be destroyed on termination
-	static StringMap& getFileHashes() {
+	inline StringMap& getFileHashes() {
 		static auto* map = new StringMap; // first: checksum, second: filePath
 		return *map;
 	};
-	static std::map<std::string, StringMapMap>& getSceneryMaps() {
+	inline std::map<std::string, StringMapMap>& getSceneryMaps() {
 		static auto* map = new std::map<std::string, StringMapMap>;
 		return *map;
 	};
-	static std::map<std::string, Map*>& getMapMap() {
+	inline std::map<std::string, Map*>& getMapMap() {
 		static auto* map = new std::map<std::string, Map*>;
 		return *map;
 	};
-	static std::map<std::string, StringMapMap>& getGameObjectMaps() {
+	inline std::map<std::string, StringMapMap>& getGameObjectMaps() {
 		static auto* map = new std::map<std::string, StringMapMap>;
 		return *map;
 	};
-	static std::map<std::string, sf::Texture*>& getTextureMap() {
+	inline std::map<std::string, sf::Texture*>& getTextureMap() {
 		static auto* map = new std::map<std::string, sf::Texture*>;
 		return *map;
 	};
-	static std::map<std::string, sf::Image*>& getImageMap() {
+	inline std::map<std::string, sf::Image*>& getImageMap() {
 		static auto* map = new std::map<std::string, sf::Image*>;
 		return *map;
 	};
--- source/util/Helper.h
+++ source/util/Helper.h
@@ -20,49 +20,42 @@
 
 #include "physics/IEMath.h"
 
-class Helper
+namespace Helper
 {
-public:
-	// Not posible to create Objects of this class
-	Helper() = delete;
-	~Helper() {};
-	Helper(Helper const&) = delete;
-	void operator=(Helper const&) = delete;
-
 	// return time since epoch (1970?) in seconds
-	static long now();
-	static std::string parseFileName(std::string path);
+	long now();
+	std::string parseFileName(std::string path);
 
-	static void readDirectory(tinydir_dir& dir, std::function<void(tinydir_file& file)> fn, bool recursive = false);
-	static void readDirectory(std::string path, std::function<void(tinydir_file& file)> fn, bool recursive = false);
+	void readDirectory(tinydir_dir& dir, std::function<void(tinydir_file& file)> fn, bool recursive = false);
+	void readDirectory(std::string path, std::function<void(tinydir_file& file)> fn, bool recursive = false);
 
-	static void readFileBytes(std::string file, std::function<void(size_t length, char* data)> fn);
-	static void writeFileBytes(std::string file, size_t length, char* data);
+	void readFileBytes(std::string file, std::function<void(size_t length, char* data)> fn);
+	void writeFileBytes(std::string file, size_t length, char* data);
 
-	static std::string replaceIllegalCharacters(std::string s, std::string illegalCharacters, char replace);
-	static void replaceAll(std::string& s, std::string toReplace, std::string replacement);
-	static std::string trim(const std::string& s, const std::string& whitespace = " \t");
+	std::string replaceIllegalCharacters(std::string s, std::string illegalCharacters, char replace);
+	void replaceAll(std::string& s, std::string toReplace, std::string replacement);
+	std::string trim(const std::string& s, const std::string& whitespace = " \t");
 
 	// static convert fn
-	static std::vector<std::string> split(std::string str, char splitter);
+	std::vector<std::string> split(std::string str, char splitter);
 	// splits by "," and converts with given fn.
 	// if length is set, an error is thrown if the tupel has an other length
 	template<typename T>
-	static std::vector<T> toTupel(std::string str, T (*conv)(std::string), size_t length = 0);
-	static int toInt(std::string s);
-	static long toLong(std::string s);
-	static float toFloat(std::string s);
-	static float toFloat(std::string s, float fallbackValue);
-	static double toDouble(std::string s);
-	static sf::Color toColor(std::string s);
-	static sf::Vector2f toVector2f(std::string s);
-	static sf::IntRect toIntRect(std::string s);
-	static sf::FloatRect toFloatRect(std::string s);
+	std::vector<T> toTupel(std::string str, T (*conv)(std::string), size_t length = 0);
+	int toInt(std::string s);
+	long toLong(std::string s);
+	float toFloat(std::string s);
+	float toFloat(std::string s, float fallbackValue);
+	double toDouble(std::string s);
+	sf::Color toColor(std::string s);
+	sf::Vector2f toVector2f(std::string s);
+	sf::IntRect toIntRect(std::string s);
+	sf::FloatRect toFloatRect(std::string s);
 	// input should be true or false
-	static bool toBool(std::string s);
+	bool toBool(std::string s);
 
-	static sf::Vector2f toSfVec(ph::Vec2 v);
-	static ph::Vec2 toPhVec(sf::Vector2f v);
+	sf::Vector2f toSfVec(ph::Vec2 v);
+	ph::Vec2 toPhVec(sf::Vector2f v);
 };
 
 #endif // SWONE_UTIL_HELPER_H
