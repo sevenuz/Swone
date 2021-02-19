b0aa7f9dd05a661e24f71095c5b606fa9ae6475e
sevenuz <julius@athenstaedt.net>
2021-02-19 17:13:02 +0100

    local menu not implemented message

--- source/client/menu/LocalMenu.cpp
+++ source/client/menu/LocalMenu.cpp
@@ -4,93 +4,28 @@ LocalMenu::LocalMenu(Controller& c) :
 	m_ps(100),
 	m_controller(c)
 {
+	m_header.setFont(m_controller.getSettings().getFont());
+	m_header.setString("Local");
+	m_header.setCharacterSize(m_controller.getSettings().toF(50));
+	m_header.setStyle(sf::Text::Bold | sf::Text::Underlined);
+	m_header.setFillColor(sf::Color::Red);
+	m_header.setPosition(m_controller.getSettings().toW(0.35f), m_controller.getSettings().toH(40));
+
+	auto gb = m_header.getGlobalBounds();
 	m_ps.setColor(sf::Color::White);
 	m_ps.setDrawingType(sf::Quads);
 	m_ps.setLifetime(sf::seconds(3));
-	m_ps.setOrigin(m_controller.getSettings().toW(0.4f), m_controller.getSettings().toH(61), 150, 3, Origin::ON_BORDER);
-
-	m_play.setFont(m_controller.getSettings().getFont());
-	m_play.setString("Play");
-	m_play.setPosition(m_controller.getSettings().toW(0.415f), m_controller.getSettings().toH(0.8f));
-	m_play.setFillColor(sf::Color::Yellow);
-	m_play.setCharacterSize(m_controller.getSettings().toF(50));
-
-	m_sceneryName.setFont(m_controller.getSettings().getFont());
-	m_sceneryName.setPosition(m_controller.getSettings().toW(0.4f), m_controller.getSettings().toH(31));
-	m_sceneryName.setFillColor(sf::Color::White);
-	m_sceneryName.setCharacterSize(m_controller.getSettings().toF(30));
-
-	m_switchLeft.setFont(m_controller.getSettings().getFont());
-	m_switchLeft.setString("<");
-	m_switchLeft.setPosition(m_controller.getSettings().toW(30), m_controller.getSettings().toH(150));
-	m_switchLeft.setFillColor(sf::Color::Yellow);
-	m_switchLeft.setCharacterSize(m_controller.getSettings().toF(50));
-
-	m_switchRight.setFont(m_controller.getSettings().getFont());
-	m_switchRight.setString(">");
-	m_switchRight.setPosition(m_controller.getSettings().toW(590), m_controller.getSettings().toH(150));
-	m_switchRight.setFillColor(sf::Color::Yellow);
-	m_switchRight.setCharacterSize(m_controller.getSettings().toF(50));
-
-	readSceneries(m_controller.getSettings().getResourceDirectory());
-	m_sceneriesFound = m_sceneries.size() > 0;
-	if(m_sceneriesFound)
-		setScenerySelection(0);
-
-	for(size_t i = 0; i < m_sceneries.size(); i++) {
-		for(auto& p : m_sceneries[i]->getPlayerSetupMaps()) {
-			m_gamePlayers[i].push_back({p.first, p.second[Reader::DEFAULT_PARAGRAPH][GameObject::S_NAME], true});
-		}
-	}
+	m_ps.setOrigin(gb.left, gb.top, gb.width, gb.height, Origin::ON_BORDER);
 }
 
 LocalMenu::~LocalMenu() {}
 
-void LocalMenu::readSceneries(std::string resDir)
-{
-	GameReader::readSceneryMaps(resDir);
-	for(auto& p : GameReader::getSceneryMaps()) {
-		Scenery* s = new Scenery(resDir, Helper::parseFileName(p.first), p.second);
-		m_sceneries.push_back(s);
-	}
-}
-
-void LocalMenu::setScenerySelection(int i) {
-	if (!m_sceneriesFound) {
-		Log::ger().log("no sceneries found.", Log::Label::Error);
-		return;
-	}
-
-	if (i >= (int)m_sceneries.size()) {
-		i = 0;
-	}
-	else if (i < 0) {
-		i = m_sceneries.size() - 1;
-	}
-
-	m_selectedScenery = i;
-
-	m_sceneryName.setString(m_sceneries[m_selectedScenery]->getName());
-}
-
-void LocalMenu::setActionSelection(char i) {
-	if (i > GamePanelMenuPoint::LAST) {
-		i = GamePanelMenuPoint::FIRST;
-	}
-	else if (i < GamePanelMenuPoint::FIRST) {
-		i = GamePanelMenuPoint::LAST;
-	}
-	m_selectedAction = i;
-}
-
-void LocalMenu::startGame() {
-	Log::ger().log("No Scenery. Can't start the game.", Log::Label::Error);
-}
-
 void LocalMenu::event(sf::Event& event) {
-	sf::RenderWindow& w = m_controller.getWindow();
-	sf::Vector2i pixelPos = sf::Mouse::getPosition(w);
-	sf::Vector2f worldPos = w.mapPixelToCoords(pixelPos);
+	if (event.type == sf::Event::KeyPressed) {
+		if (event.key.code == sf::Keyboard::Escape) {
+			m_controller.popState();
+		}
+	}
 }
 
 void LocalMenu::update(sf::Time ellapsed) {
@@ -99,25 +34,28 @@ void LocalMenu::update(sf::Time ellapsed) {
 
 void LocalMenu::drawImgui()
 {
-
 	// imgui.h:595 or imgui_demo.cpp:187
-	int window_flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoResize |
-											ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoNav;
+	int window_flags = ImGuiWindowFlags_NoMove |
+		ImGuiWindowFlags_NoSavedSettings |
+		ImGuiWindowFlags_NoResize |
+		ImGuiWindowFlags_NoCollapse |
+		ImGuiWindowFlags_NoNav |
+		ImGuiWindowFlags_NoTitleBar;
 	bool* w_open = NULL; // hides close option
 
-	auto player_pos = ImVec2(m_controller.getSettings().toW(0.1f), m_controller.getSettings().toH(0.1f));
-	auto window_size = ImVec2(m_controller.getSettings().toW(0.2f), m_controller.getSettings().toH(0.8f));
+	auto window_pos = ImVec2(m_controller.getSettings().toW(0.1f), m_controller.getSettings().toH(0.3f));
+	auto window_size = ImVec2(m_controller.getSettings().toW(0.8f), m_controller.getSettings().toH(0.6f));
 
-	std::string no_files_found = "No Files of this type found.";
-
-	ImGui::SetNextWindowPos(player_pos, ImGuiCond_Always);
+	ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always);
 	ImGui::SetNextWindowSize(window_size, ImGuiCond_Always);
-	ImGui::Begin("Select Players", w_open, window_flags);
-	if(m_gamePlayers[m_selectedScenery].size() == 0)
-		ImGui::TextColored(ImColor(210, 0, 0), no_files_found.c_str());
-	else
-		for(GameObjectSelection& gos : m_gamePlayers[m_selectedScenery])
-			ImGui::Selectable(gos.name.c_str(), &gos.selected);
+	ImGui::Begin("Local", w_open, window_flags);
+		// TODO imgui settings menu
+		ImGui::TextColored(ImColor(210, 0, 0), "LocalGame is not implemented yet. BUT");
+		ImGui::TextColored(ImColor(210, 0, 0), "With following steps, you can play local:");
+		ImGui::BulletText("go into your settings.ini");
+		ImGui::BulletText("change server entry to: localhost");
+		ImGui::BulletText("change port entry to: 61007");
+		ImGui::BulletText("Play over the online menu and host your game locally :)");
 	ImGui::End();
 }
 
@@ -125,5 +63,8 @@ void LocalMenu::draw(sf::RenderTarget& target, sf::RenderStates states) const {
 	states.transform *= getTransform();
 	states.texture = NULL;
 
-};
+	m_controller.setDefaultView();
 
+	target.draw(m_ps, states);
+	target.draw(m_header, states);
+};
--- source/client/menu/LocalMenu.h
+++ source/client/menu/LocalMenu.h
@@ -12,16 +12,11 @@
 #include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics/Color.hpp>
 
-#include <map>
 #include <vector>
 #include <string>
 #include <sstream>
 
-#include <tinydir.h>
-
 #include "graphics/ParticleSystem.h"
-#include "game/GameController.h"
-#include "game/GameReader.h"
 #include "Handleable.h"
 #include "client/Settings.h"
 #include "client/Controller.h"
@@ -29,52 +24,21 @@
 #include "util/Helper.h"
 
 class LocalMenu : public Handleable {
-private:
-	enum GamePanelMenuPoint : char { PLAY, FIRST = PLAY, LAST = PLAY };
-
-	ParticleSystem m_ps;
-	Controller& m_controller;
-
-	sf::Text m_play;
-
-	sf::Text m_sceneryName;
-	sf::Text m_switchLeft;
-	sf::Text m_switchRight;
-
-	// GamePanelMenuPoint type, but char to perform de/increment
-	char m_selectedAction = FIRST;
-	int m_selectedScenery = 0;
-	bool m_sceneriesFound = false;
-
-	struct GameObjectSelection {
-		std::string key;
-		std::string name;
-		bool selected;
-	};
-	std::vector<Scenery*> m_sceneries;
-	std::map<int, std::vector<GameObjectSelection>> m_gamePlayers; // key: m_selectedScenery
-
-	void readSceneries(std::string resDir);
-
-	void setScenerySelection(int);
-	void setActionSelection(char);
-
-	void startGame();
-
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-
 public:
 	void drawImgui();
 
 	void update(sf::Time ellapsed);
 	void event(sf::Event& e);
+
 	LocalMenu(Controller& c);
 	~LocalMenu();
+private:
+	ParticleSystem m_ps;
+	Controller& m_controller;
 
-	const GameController& getGameController() const;
-	GameObject* getGameObjectById(const std::string& id) const;
+	sf::Text m_header;
 
-	void updateLog() const;
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
 
 #endif
